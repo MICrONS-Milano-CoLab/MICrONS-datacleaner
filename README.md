3b42d4ba410b506888a4cc873add1b7f874233f3
VictorSeven <vbuendiar@gmail.com>
2025-04-01 16:27:12 +0200

    Created basic structure for the project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MICrONS-datacleaner
+
+# Requirements 
+
+- CaveCLIENT
+- H5Py
+- Standard transform for coordinate change
---
+++ source/downloader.py
@@ -0,0 +1,6 @@
+from caveclient import CAVEclient
+from standard_transform import minnie_transform_vx
+
+import numpy as np
+import pandas as pd
+
